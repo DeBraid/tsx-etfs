7f26124b89f5574c6e5d649e77f93ac6d4ad7bc8
DeBraid <de.braid@gmail.com>
2015-02-22 10:54:51 -0500

    added spinner and logic for displaying it

--- client/main.js
+++ client/main.js
@@ -1,1 +1,29 @@
-XIU = new Mongo.Collection('xiu');
+// XIU = new Mongo.Collection('xiu');
+
+Template.waiting.helpers({
+  waitingIs: function () {
+    var loading = Session.get('inputState');
+    return loading == 'has-success' ? false : true ; 
+  }
+});
+
+Template.waiting.created = function () {
+  Meteor.Spinner.options = {
+    lines: 8, // The number of lines to draw
+    length: 8, // The length of each line
+    width: 2, // The line thickness
+    radius: 10, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0.5, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#F00', // #rgb or #rrggbb
+    speed: 0.8, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: true, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    right: 'auto' // Left position relative to parent in px
+  };
+};
