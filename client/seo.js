c4c71639b982776853a43bf42bae247a5b2caf6d
DeBraid <de.braid@gmail.com>
2015-08-03 19:47:09 -0400

    added twitter cards via seo pkg

--- client/seo.js
+++ client/seo.js
@@ -7,6 +7,14 @@ Meteor.startup(function() {
             },
             og: {
                 'image': 'http://static01.nyt.com/images/2015/08/04/world/04BURUNDI/04BURUNDI-master675.jpg' 
+           },
+            twitter: {
+                'card' : 'summary_large_image',
+                'site' : '@Royal_Arse',
+                'creator' : '@Royal_Arse',
+                'title' : 'Stock and or ETF Info.',
+                'description' : 'Charts for stocks and etfs on TSX.  Canadian-based securities.',
+                'image' : 'http://imgur.com/gallery/lFOj8oz'
            }
         });
     }
